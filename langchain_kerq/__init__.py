3b60d2535eef3d32c7e70f03c136872f42dd4c05
greenoriginals <greenoriginals@gmail.com>
2026-04-23 00:45:46 -0500

    Bump version to 0.1.1

--- langchain_kerq/__init__.py
+++ langchain_kerq/__init__.py
@@ -4,4 +4,4 @@ from langchain_kerq.tools import KerqTrustTool
 from langchain_kerq.callbacks import KerqTelemetryHandler, KerqGuard
 
 __all__ = ["KerqTrustTool", "KerqTelemetryHandler", "KerqGuard"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
